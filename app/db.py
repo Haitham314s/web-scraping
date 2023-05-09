7d77ee39e5f12c6cd7dbdc73aba1981591066431
Haitham <haitham314s@gmail.com>
2023-05-09 09:24:24 +0400

    init files + db connection

---
+++ app/db.py
@@ -0,0 +1,32 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+import pathlib
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ASTRA_DB_CLIENT_ID = os.getenv("ASTRA_DB_CLIENT_ID")
+ASTRA_DB_CLIENT_SECRET= os.get_env("ASTRA_DB_CLIENT_SECRET")
+
+BASE_DIR = pathlib.Path(__file__).parent
+CLUSTER_BUNDLE = str(BASE_DIR / "ignored" / "connect.zip")
+
+
+def get_cluster():
+    cloud_config = {
+        'secure_connect_bundle': CLUSTER_BUNDLE
+    }
+    auth_provider = PlainTextAuthProvider(ASTRA_DB_CLIENT_ID, ASTRA_DB_CLIENT_SECRET)
+    return Cluster(cloud=cloud_config, auth_provider=auth_provider)
+
+
+def get_session():
+    cluster = get_cluster()
+    return cluster.connect()
+
+
+session = get_session()
+if row := session.execute("select release_version from system.local").one():
+    print(row[0])
+else:
+    print("An error occurred.")
